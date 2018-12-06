d2831e88c4df7f58605353bd34cef62e5d9dec75
stefan <stefan@betech.rs>
2018-12-06 14:22:15 +0100

    update deploy file

--- deploy.php
+++ deploy.php
@@ -2,7 +2,7 @@
 
 namespace Deployer;
 
-require 'recipe/magento_2_2.php';
+require 'magento/vendor/jalogut/magento2-deployer-plus/recipe/magento_2_2_5.php';
 
 // Use timestamp for release name
 set('release_name', function () {
@@ -12,13 +12,12 @@ set('release_name', function () {
 // Magento dir into the project root. Set "." if magento is installed on project root
 set('magento_dir', 'magento');
 // [Optional] Git repository. Only needed if not using build + artifact strategy
-set('repository', 'https://github.com/jalogut/magento2-project-template-2.2.git');
-
+set('repository', '');
 // Space separated list of languages for static-content:deploy
-set('languages', 'en_US de_CH fr_FR');
+set('languages', 'en_US');
 
 // OPcache configuration
-task('cache:clear:opcache', 'sudo /usr/sbin/service php7.1-fpm reload');
+task('cache:clear:opcache', 'sudo systemctl reload php-fpm');
 after('cache:clear', 'cache:clear:opcache');
 
 // Build host
@@ -26,15 +25,42 @@ localhost('build');
 
 // Remote Servers
 host('dev_master')
-    ->hostname('<dev_hostname>')
-    ->user('<dev_user>')
+    ->hostname('<hostname>')
+    ->user('<user>')
     ->set('deploy_path', '~')
     ->stage('dev')
     ->roles('master');
 
+host('stage_master')
+    ->hostname('<hostname>')
+    ->user('<user>')
+    ->set('deploy_path', '~')
+    ->stage('stage')
+    ->roles('master');
+
 host('prod_master')
-    ->hostname('<prod_hostname>')
-    ->user('<prod_user>')
+    ->hostname('<hostname>')
+    ->user('<user>')
     ->set('deploy_path', '~')
     ->stage('prod')
     ->roles('master');
+
+// ---- Multi-server Configuration ----
+// Tasks available only for specific roles
+// task('config:import')->onRoles('master');
+// task('database:upgrade')->onRoles('master');
+// task('crontab:update')->onRoles('master');
+//
+//host('prod_slave_1')
+//    ->hostname('<hostname>')
+//    ->user('<user>')
+//    ->set('deploy_path', '~')
+//    ->stage('prod')
+//    ->roles('slave');
+//
+//host('prod_slave_2')
+//    ->hostname('<hostname>')
+//    ->user('<user>')
+//    ->set('deploy_path', '~')
+//    ->stage('prod')
+//    ->roles('slave');
